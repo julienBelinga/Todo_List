8a5c496340a9caa3ccf9e7fcc91b355af7d8348b
julien belinga <julienbelinga29@gmail.com>
2024-12-12 18:10:25 +0100

    feat: enhance task management by clearing input field after saving and adding border radius to delete button

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -26,6 +26,7 @@ class _HomePageState extends State<HomePage> {
   void saveNewTask() {
     setState(() {
       todoList.add([_controller.text, false]);
+      _controller.clear();
     });
     Navigator.of(context).pop();
   }
